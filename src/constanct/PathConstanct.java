823eac0608a27f378da7369eed21a1791ececa1f
pdhung3012 <pdhung3012@gmail.com>
2020-04-14 22:33:34 -0500

    update paths

--- src/constanct/PathConstanct.java
+++ src/constanct/PathConstanct.java
@@ -5,7 +5,7 @@ public class PathConstanct {
 	public static String PATH_JAVA_CLASSPATH = "C:\\Program Files\\Java\\jdk1.8.0_144\\jre\\lib\\rt.jar";
 	
 	
-	public static String PATH_INPUT_PROJECTS="G:\\gitAlon18Projects\\raw\\java-large\\training\\";
+	public static String PATH_INPUT_PROJECTS="G:\\gitAlon18Projects\\raw\\java-large\\validation\\";
 	public static String PATH_OUTPUT_TEXT="G:\\gitAlon18Projects\\starPredictCodeInfos\\";
 	public static String PATH_INPUT_PRO_LOCAL="G:\\StarPredictorProjects\\input\\";
 	public static String PATH_OUTPUT_PRO_LOCAL="G:\\StarPredictorProjects\\textual\\";
